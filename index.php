e29103e4a96b235cf8333801aad829f08e971b89
TanveerSingh09 <42763688+TanveerSingh09@users.noreply.github.com>
2021-05-21 19:47:48 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<HTML>
+<head>
+	 <meta name="author" content="Vinit Shahdeo">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="format-detection" content="telephone=no">
+      <link rel="stylesheet" href="style.css">
+      <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat|Cookie&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Delius&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" crossorigin="anonymous">
+	<center><title>Sparks Bank</title></center>
+	<link rel = "stylesheet" type = "text/css" href = "buttons.css">
+</head>
+<body style="background-color:#000000;background-attachment: fixed;background-repeat: no-repeat;background-size: cover;">
+	
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+	<div class="one" style=" background-color:#1DA1F2;
+   height: :50px;
+   width:100%;">
+		<table width="1330">
+            		
+			<table width="1200" align="center"  class = "t">
+                <tr>
+				<td style = "text-align:center"> <button class = "btn1"> Home </button></td> 	
+				<td style = "text-align:center"><a href="customer.php" target="frame"><button class = "btn2">View Customers</button></a></td>
+				<td style = "text-align:center"><a href="Transfermoney.php" target="frame"><button class = "btn2">Transfer Money</button></a></td>
+				<td style = "text-align:center"><a href="transactionhistory.php" target="frame"><button class = "btn2">View Transaction History</button></a></td>
+                </tr>
+            </table>
+        </table>
+	</div>
+	<div class="three">Welcome to</div>
+	<h1 >
+	
+
+<span style="color: #1DA1F2;">S</span>
+<span style="color: #1DA1F2;">E</span>
+<span style="color: #1DA1F2;">A</span>
+<span style="color: #1DA1F2;">W</span>
+<span style="color: #1DA1F2;">A</span>
+<span style="color: #1DA1F2;">Y</span>
+<span style="color: white;">B</span>
+<span style="color: white;">A</span>
+<span style="color: white;">N</span>
+<span style="color: white;">K</span>
+</h1>
+	<div class="three" >This Basic Banking System helps to transfer money between users .<br> It's a simple process , you just need to click on users name and it will automatically jump to next page<br> where you need to select the user to whom you want to transfer money and the amount you want to transfer.<br>
+	<div class="four"> Now just click the submit button! YOUR  TRANSACTION  IS  SUCCESSFUL:)</div> 
+</div>
+<div class="five">Designed By Tanveer || As a part of THE SPARKS FOUNDATION internship.<br> Have any Query? Feel free to Contact:
+</div>
+      <span style="color: red; padding: 20px;">
+          <br>        
+         <a style="margin-left: 600px;color:wheat;" href="https://twitter.com/tanukahlon" onmouseover="this.style.color='#1DA1F2'" onmouseout="this.style.color='white'"><i class="fab fa-twitter fa-2x"></i></a>
+         <a style="margin-left: 10px;color:wheat;"href="https://github.com/TanveerSingh09" onmouseover="this.style.color='#1DA1F2'" onmouseout="this.style.color='white'" ><i class="fab fa-github fa-2x"></i></a>
+         <a style="margin-left:10px;color:wheat;"href="https://www.linkedin.com/in/tanveer-singh-754b68143/" onmouseover="this.style.color='#1DA1F2'" onmouseout="this.style.color='white'"><i class="fab fa-linkedin fa-2x"></i></a>
+         <a style="margin-left: 10px;color:wheat;"href="https://www.instagram.com/tanveerskahlon/" onmouseover="this.style.color='#1DA1F2'" onmouseout="this.style.color='white'"><i class="fab fa-instagram fa-2x"></i></a>
+         </span> 
+         </div>
+</body>
+</html>
